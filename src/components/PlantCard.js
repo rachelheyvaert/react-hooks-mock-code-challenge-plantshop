fe942f59ca59cb542c4dcdee107bb28ae6f757bb
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-05-09 08:50:30 -0400

    sold out buttons

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
 function PlantCard({ plantList }) {
+  const [inStock, setIsInStock] = useState(true)
   const { name, image, price} = plantList;
   return (
     <li className="card">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {inStock ? (
+        <button onClick={()=> setIsInStock(false)} className="primary">In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={()=> setIsInStock(true)}>Out of Stock</button>
       )}
     </li>
   )}
